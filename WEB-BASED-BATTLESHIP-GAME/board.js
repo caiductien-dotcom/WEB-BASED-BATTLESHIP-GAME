1ccf459ddb947b4f998f9a12ef33428d0ad3146e
haduyminh5a1 <haduyminh5a1@gmail.com>
2026-02-25 21:29:11 +0700

    use constants to create board

--- WEB-BASED-BATTLESHIP-GAME/board.js
+++ WEB-BASED-BATTLESHIP-GAME/board.js
@@ -1,9 +1,11 @@
+import { board_size } from "./constants";
+
 export function createBoatData() {
     let board = [];
 
-    for (let i=0;i<10;i++){
+    for (let i=0;i<board_size;i++){
         board[i] = [];
-        for (let j=0;j<10;j++){
+        for (let j=0;j<board_size;j++){
             board[i][j] = 0;
         }
     }
