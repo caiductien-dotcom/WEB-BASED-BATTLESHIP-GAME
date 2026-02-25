d8a0e49779e4116ca35723b1f5a940067600609f
caiductien-dotcom <caiductien@gmail.com>
2026-02-25 22:33:32 +0700

    add ui.js, add style.css change game.js and index.html

--- WEB-BASED-BATTLESHIP-GAME/game.js
+++ WEB-BASED-BATTLESHIP-GAME/game.js
@@ -1,6 +1,11 @@
 import { createBoatData } from "./board.js";
+import { renderBoard } from "./ui.js";
 
 const pBoard = createBoatData();
+const cBoard = createBoatData();
 
 pBoard[0][0] = 1;
 pBoard[1][1] = 2;
+
+renderBoard(pBoard, 'player-board');
+renderBoard(cBoard, 'cpu-board');
---
+++ WEB-BASED-BATTLESHIP-GAME/ui.js
@@ -0,0 +1,30 @@
+import { cell_types } from "./constants.js";
+export function renderBoard(data,containerId) {
+    const boardElement = document.getElementById(containerId);
+    if (!boardElement) return;
+    boardElement.innerHTML = "";
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+            const cellValue = data[i][j];
+            const cellElement = document.createElement("div");
+            cellElement.classList.add("cell");
+            switch (cellValue) {
+                case cell_types.empty:
+                    cellElement.classList.add("empty");
+                    break;
+                case cell_types.ship:
+                    cellElement.classList.add("ship");
+                    break;
+                case cell_types.hit:
+                    cellElement.classList.add("hit");
+                    break;
+                case cell_types.miss:
+                    cellElement.classList.add("miss");
+                    break;
+                default:
+                    break;
+            }
+            boardElement.appendChild(cellElement);
+        }
+    }
+}
