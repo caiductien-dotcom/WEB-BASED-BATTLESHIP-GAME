62b438d3a54b7fd4ca68dfb5a952cc14d5374957
haduyminh5a1 <haduyminh5a1@gmail.com>
2026-02-25 21:23:00 +0700

    update

---
+++ WEB-BASED-BATTLESHIP-GAME/board.js
@@ -0,0 +1,10 @@
+export function createBoatData() {
+    let board = [];
+
+    for (let i=0;i<10;i++){
+        board[i] = [];
+        for (let j=0;j<10;j++){
+            board[i][j] = 0;
+        }
+    }
+}
---
+++ WEB-BASED-BATTLESHIP-GAME/game.js
@@ -0,0 +1,6 @@
+import { createBoatData } from "./board";
+
+const pBoard = createBoatData();
+
+pBoard[0][0] = 1;
+pBoard[1][1] = 2;
